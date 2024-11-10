4ba5fb8b5edb0a503cfa183c45cee48b53a8ff56
darya <uakaama@gmail.com>
2024-11-10 22:59:10 +0200

    logging

--- run_all.py
+++ run_all.py
@@ -1,10 +1,13 @@
 from flask import Flask
 import subprocess
+import logging
+logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
 
 @app.route('/check_stories', methods=['GET'])
 def check_stories():
+    logging.info("Endpoint /check_stories was triggered.")
     try:
         # Run instaLoader.py to download stories
         subprocess.run(["python", "instaLoader.py"])
@@ -12,8 +15,9 @@ def check_stories():
         # Run StoriesChannel.py to post downloaded stories
         subprocess.run(["python", "StoriesChannel.py"])
 
-        return "Stories checked and posted if any updates found.", 200
+        return "Stories checked and posted if any updates found./n  t.me/uakaamaStories", 200
     except Exception as e:
+        logging.error(f"An error occurred while checking stories: {e}")
         return f"Error: {str(e)}", 500
 
 if __name__ == "__main__":
