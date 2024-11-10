756aaa655ce7280f6ca39b5317b878247215c9d7
darya <uakaama@gmail.com>
2024-11-10 14:50:17 +0200

    update python paths

--- run_all.py
+++ run_all.py
@@ -1,6 +1,6 @@
 import subprocess
 # Run instaLoader.py to download stories
-subprocess.run(["pythonProject/.venv/bin/python", "instaLoader.py"])
+subprocess.run(["python", "instaLoader.py"])
 
 # Run StoriesChannel.py to post downloaded stories
-subprocess.run(["pythonProject/.venv/bin/python", "StoriesChannel.py"])
+subprocess.run(["python", "StoriesChannel.py"])
